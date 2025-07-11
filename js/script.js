34f7b0f1b41ded2e0b82f99e281853e8897a6991
Marco Bacci <marcobacci94@gmail.com>
2025-07-11 18:09:33 +0200

    fix: cicle for not to repeat random numbers. feat: cicle for to visualize numbers in <li> on page

--- js/script.js
+++ js/script.js
@@ -1,40 +1,44 @@
-// recupero elementi del dom: button per avviare la l'evento, countdown e numeri random, form, input 
-const button = document.getElementById('message')
-const countDown = document.getElementById('countdown')
+// recupero elementi del dom: button per avviare la l'evento, countdown e numeri random, form, input
+const button = document.getElementById("message");
+const countDown = document.getElementById("countdown");
 const instructions = document.getElementById("instructions");
-const randomNumList = document.getElementById('numbers-list')
-const form = document.getElementById('answers-form')
+const randomNumList = document.getElementById("numbers-list");
+const form = document.getElementById("answers-form");
 
-
-// creo una funzione che mi genera i numeri random 
-function generateRandomNumbers(){
+// creo una funzione che mi genera i numeri random
+function generateRandomNumbers() {
   const num = [];
-  for (let i = 0; i < 5; i++){
-    const random= Math.floor(Math.random() * 30) + 1;
-    num.push(random)
+  for (let i = 0; num.length < 5; i++) {
+    const random = Math.floor(Math.random() * 30) + 1;
+    if(!num.includes(random)){
+      num.push(random);
+    }
+
+
   }
-  return num
+  return num;
 }
-const randomNumbers = generateRandomNumbers()
-randomNumList.innerHTML = randomNumbers
+const randomNumbers = generateRandomNumbers();
 
-// definisco intervallo e controllo se i numeri sono uguali a 0 e in quel caso faccio apparire il form 
+// ciclo per visualizzare i numeri
 
-let seconds = 10
+for (let i = 0; i < 5; i++) {
+  randomNumList.innerHTML += `<li>${randomNumbers[i]}</li>`;
+}
 
+// definisco intervallo e controllo se i numeri sono uguali a 0 e in quel caso faccio apparire il form
 
-const intervalId = setInterval(() =>{
-  countDown.innerText = seconds;
+let seconds = 10;
+
+const intervalId = setInterval(() => {
   seconds--;
-  if(seconds === 0){
-    clearInterval(intervalId)  
-    form.classList.remove('d-none') 
-    countDown.classList.add('d-none')
-    randomNumList.classList.add('d-none')
-    
-    instructions.innerText = 'Inserisci tutti i numeri che ricordi'
+  countDown.innerText = seconds;
+  if (seconds === 0) {
+    clearInterval(intervalId);
+    form.classList.remove("d-none");
+    countDown.classList.add("d-none");
+    randomNumList.classList.add("d-none");
+
+    instructions.innerText = "Inserisci tutti i numeri che ricordi";
   }
-  
 }, 1000);
-
-
